dd58a4e700c09b3dec62a432104b1ba0a0deae17
unknown <>
2019-06-17 13:57:15 +0800

    working

---
+++ dbdb/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+
+# dbdb/__init__.py
+def connect(dbname):
+    try:
+        f = open(dbname, 'r+b')
+    except IOError:
+        fd = os.open(dbname, os.O_RDWR | os.O_CREAT)
+        f = os.fdopen(fd, 'r+b')
+    return DBDB(f)
